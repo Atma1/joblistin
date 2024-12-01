3f1556796430e91adab62ff4e8bd8412d798640d
Atma1 <atmasusanto08@gmail.com>
2024-12-01 17:44:54 +0800

    removed

--- resources/views/job.blade.php
+++
@@ -1,9 +0,0 @@
-<x-layout>
-    <x-slot:heading>
-        Job
-    </x-slot:heading>
-    <h2 class="font-bold text-lg">Job Title: {{ $job["title"] }}</h2>
-    <p>
-        Salary: {{ $job["salary"] }}
-    </p>
-</x-layout>
