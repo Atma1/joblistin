248f0146a0b62237275288626902b82e23b4d15e
Atma1 <atmasusanto08@gmail.com>
2024-12-01 17:44:28 +0800

    feat: cleaned up router

--- routes/web.php
+++ routes/web.php
@@ -1,25 +1,9 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
-use App\Models\Job;
+use App\Http\Controllers\JobController;
 
-Route::get('/', function () {
-    return view('home', [
-        "greetings" => "Hello"
-    ]);
-});
+Route::view('/', 'home');
+Route::view('/contact', 'contact');
 
-Route::get('/jobs', function () {
-    $jobs = Job::with('employer')->simplePaginate(3);
-    return view('jobs', ["jobs" => $jobs]);
-});
-
-Route::get('/jobs/{id}', function ($id) {
-    $job = Job::find($id);
-
-    return view('job', ['job' => $job]);
-});
-
-Route::get('/contact', function () {
-    return view('contact');
-});
+Route::resource('jobs', JobController::class);
