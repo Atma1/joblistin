4ce745016f06735209e9b210c37dcb227cd23cd3
Atma1 <atmasusanto08@gmail.com>
2024-12-20 16:37:27 +0800

    feat: made active props default to false

--- resources/views/components/nav-link.blade.php
+++ resources/views/components/nav-link.blade.php
@@ -1,7 +1,5 @@
-@props(['activate' => true])
+@props(['active' => false])
 
-@if($activate === true)
-<a class="{{ $activate ? 'bg-gray-900 text-white' :  'text-gray-300 hover:bg-gray-700 hover:text-white' }} rounded-md px-3 py-2 text-sm font-medium"
+<a class="{{ $active ? 'bg-gray-900 text-white' :  'text-gray-300 hover:bg-gray-700 hover:text-white' }} rounded-md px-3 py-2 text-sm font-medium"
     {{ $attributes }}>{{ $slot }}
 </a>
-@endif
