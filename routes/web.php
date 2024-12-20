68e3ddafb47d17511d31c1d71ed9cbc625379130
Atma1 <atmasusanto08@gmail.com>
2024-12-20 16:37:39 +0800

    feat: use controller

--- routes/web.php
+++ routes/web.php
@@ -2,8 +2,16 @@
 
 use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\JobController;
+use App\Http\Controllers\RegisterController;
+use App\Http\Controllers\SessionController;
 
 Route::view('/', 'home');
 Route::view('/contact', 'contact');
+
+Route::get('/register', [RegisterController::class, 'create']);
+Route::post('/register', [RegisterController::class, 'store']);
+Route::get('/login', [SessionController::class, 'create']);
+Route::post('/login', [SessionController::class, 'store']);
+Route::post('/logout', [SessionController::class, 'destroy']);
 
 Route::resource('jobs', JobController::class);
